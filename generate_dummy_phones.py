634614c090c4c167ae25e611c9f2638644e90ff7
nottherealtar <67793100+nottherealtar@users.noreply.github.com>
2023-11-21 09:34:18 +0200

    -V1 Main Commit, new phone numbers generator.

---
+++ generate_dummy_phones.py
@@ -0,0 +1,47 @@
+import random
+import datetime
+
+def generate_phone_numbers(num_to_generate):
+    """
+    Generate a list of random South African phone numbers.
+
+    Args:
+        num_to_generate (int): Number of phone numbers to generate.
+
+    Returns:
+        list: List of randomly generated phone numbers.
+    """
+    return ['+27' + str(random.randint(710000000, 719999999)) for _ in range(num_to_generate)]
+
+def write_phone_numbers_to_file(phone_numbers, filename):
+    """
+    Write a list of phone numbers to a file.
+
+    Args:
+        phone_numbers (list): List of phone numbers to write.
+        filename (str): Name of the file to which the phone numbers will be written.
+    """
+    with open(filename, 'w') as file:
+        file.writelines('\n'.join(phone_numbers))
+
+def main():
+    """
+    Main function to execute the script.
+    """
+    # Get user input for the number of phone numbers to generate
+    num_to_generate = int(input("How many phone numbers would you like to generate? "))
+
+    # Generate phone numbers
+    phone_numbers = generate_phone_numbers(num_to_generate)
+
+    # Create filename with timestamp
+    now = datetime.datetime.now()
+    filename = now.strftime("%Y%m%d-%H%M%S") + "_phone_numbers.txt"
+
+    # Write phone numbers to file
+    write_phone_numbers_to_file(phone_numbers, filename)
+
+    print(f"Phone numbers written to {filename}")
+
+if __name__ == "__main__":
+    main()
